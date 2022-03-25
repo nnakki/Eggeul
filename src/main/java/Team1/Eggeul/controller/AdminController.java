676d32f2056efb270026cdbfa6412e6cce8b4181
yujinjeon1020 <yujinjeon1020@gmail.com>
2022-03-25 17:05:57 +0900

    import 수정

--- src/main/java/Team1/Eggeul/controller/AdminController.java
+++ src/main/java/Team1/Eggeul/controller/AdminController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
+//import javax.xml.ws.Response;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
