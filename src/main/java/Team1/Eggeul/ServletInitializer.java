f432f99676bda124ce2c84fa15ee76c8e692cee6
movmovmovmov <kej708@gmail.com>
2022-03-25 14:57:11 +0900

    [최초]

---
+++ src/main/java/Team1/Eggeul/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Team1.Eggeul;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EggeulApplication.class);
+	}
+
+}
